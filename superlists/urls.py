e48ad011924a9088c0f71ed90965d0f98676cb81
azam alias <azam.alias@iproperty.com>
2014-09-23 12:00:24 +0800

    added an url for a list, and used new list template

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,4 +7,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^$', 'lists.views.home_page', name='home'),
+                       url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',)
                        )
