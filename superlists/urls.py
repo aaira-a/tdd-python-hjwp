200fbf3ffb4a119283224d3936d5156ad8788bbf
azam alias <azam.alias@iproperty.com>
2014-09-23 13:54:03 +0800

    using /lists/new url for new list item, removed redundant tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,5 +7,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^$', 'lists.views.home_page', name='home'),
-                       url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',)
+                       url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list'),
+                       url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
                        )
