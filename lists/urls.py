77e31168b8ecacf73dc4394ed20846df77d41beb
azam alias <azam.alias@iproperty.com>
2014-09-23 16:17:07 +0800

    moved url from superlists to lists, using includes

---
+++ lists/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+
+urlpatterns = patterns('',
+                       url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+                       url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+                       url(r'^new$', 'lists.views.new_list', name='new_list'),
+                       )
